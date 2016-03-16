ca4dfa1e387d20dbf3cc78db681c561aa2100445
Samuel Geoffroy-Héroux <samuel.geoffroy.92@gmail.com>
2016-03-16 18:13:48 -0400

    Add controller.ino
    This file is the manager of all other headers and .cpp files
    Contains void loop()

---
+++ Control/controller/controller.ino
@@ -0,0 +1,26 @@
+/*
+ *Name 			: main.cpp 
+ *Project 		: CableCam_Chariot
+ *Description 	: 
+ *		Manage comms. and control.
+ *		Detect errors and send them to the STATE Led
+ *Created by 	: Samuel Geoffroy AKA Captnlink, March 16, 2016.
+ *Last Modif	: March 16, 2016.
+ *Released into the public domain.
+*/
+
+//Includes
+
+
+//Defines
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
