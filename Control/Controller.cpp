161f1d6d76381a44a82cc3de840b73fa43b0185f
Samuel Geoffroy-Héroux <samuel.geoffroy.92@gmail.com>
2016-03-20 12:43:22 -0400

    Correction of Controller & Tank Simulator (Test PID)

--- Control/Controller.cpp
+++ Control/Controller.cpp
@@ -1,35 +1,38 @@
-#include "GameLoop.h"
+#include "Controller.h"
 
 //Includes
 //Include default configuration
 
-Controller::Controller()
+PID_Controller::PID_Controller()
 {
-    dState; 
-	iState; 
-	iMax = 0;
-	iMin = 10;
+    dState;
+	iState;
+	//Default Configuration
+	iMax = 1000;
+	iMin = -1000;
 	iGain = 0;
 	pGain = 1;
 	dGain = 0;
 }
 
-double Controller::Update(double error, double speed){
+
+double PID_Controller::Update(double error, double feedback){
 	double pTerm, dTerm, iTerm;
-	
+
 	//Proportional term
 	pTerm = pGain * error;
-	
+
 	//Intergral term
-	iState += error;	
+	iState += error;
 	if(iState > iMax) iState = iMax;
 	else if(iState < iMin) iState = iMin;
 	iTerm = iGain * iState;
-	
+
 	//Derivate term
-	dTerm = dGain * (dState - speed);
-	dState = speed;
-	
-	return pTerm + dTerm + iTerm;	
-	
+	dTerm = dGain * (dState - feedback);
+	dState = feedback;
+
+	//Final Sommation
+	return pTerm + dTerm + iTerm;
+
 }
--- Control/Controller.h
+++ Control/Controller.h
@@ -1,36 +1,64 @@
-#ifndef Controller_H_
-    #define Controller_H_
+/**
+    Project : CableCam_Chariot\n
+    @file Controller.h
+    @brief PID_Controller class inside
 
-	/*
- *Name 			: controller 
- *Project 		: CableCam_Chariot
- *Description 	: 
- *		PID control
- *		
- *Created by 	: Samuel Geoffroy AKA Captnlink, March 19, 2016.
- *Last Modif	: March 19, 2016.
- *Released into the public domain.
+    @author Samuel Geoffroy AKA Captnlink
+    @version 1.1
+    @date March 20, 2016
+
 */
 
-//Includes
+#ifndef PID_Controller_H_
+#define PID_Controller_H_
 
-//Defines
+/**
+    @class PID_Controller Controller.h
+    @brief PID Controller
 
+    @author Samuel Geoffroy AKA Captnlink
+    @version 1.1
+    @date March 20, 2016
 
-class Controller{
-    private:
-        double  dState; //Last position
-		double  iState; //Integrator state
-		double  iMax,iMin //MAx and min allowable integrator state
-		
-		//PID Gain
-		double  iGain,
-				pGain,
-				dGain;
+    This class contain a PID controller with the respective gain for proportional, integral and derivative
+*/
+class PID_Controller
+{
+private:
+    double  dState; //Last position
+    double  iState; //Integrator state
+    double  iMax,iMin; //MAx and min allowable integrator state
 
-    public:
-        Controller();
-        double Update(double error, double speed);
+    //PID Gain
+    double  iGain; // Integral Gain
+    double  pGain; // Proportional Gain
+    double  dGain; // Derivative Gain
+
+public:
+    PID_Controller();
+
+    /**
+    @fn Update
+    @brief This fonction refresh the output of the controller
+    @param error     : setpoint - feedback
+    @param feedback  : feedback from machinery
+    @return output   : % of output
+
+    The controller use proportional, integral and derivative operation to drive an output
+
+    */
+    double Update(double error, double feedback);
+
+    //Setters
+    void setpGain(double p){pGain = p;}
+    void setiGain(double i){iGain = i;}
+    void setdGain(double d){dGain = d;}
+
+    //Getters
+    double getpGain(){return pGain;}
+    double getiGain(){return iGain;}
+    double getdGain(){return dGain;}
+
 };
 
 #endif
