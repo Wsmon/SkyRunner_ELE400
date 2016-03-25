2ad57ab706f3e779fb27a9c2fbd2d51e34f09471
Samuel Geoffroy-Héroux <samuel.geoffroy.92@gmail.com>
2016-03-24 21:45:09 -0400

    Modification BrainControl
    Start of Update()
    Start of SetVitesse()

--- Control/SR_Controller/BrainControl.cpp
+++ Control/SR_Controller/BrainControl.cpp
@@ -1,7 +1,65 @@
 BrainControl::BrainControl(){
-	
+    mPositionChariotActuel=0;
+	mPositionChariotVoulu=0;
+	mPositionActuel=0; //Position du chariot sur le cable
+	mPositionMax=0; //Position maximum du point de départ (Longueur de cable)
+	mVitesseActuel=0;
+	mVitesseVoulu=0;
+	mDistanceAvant = 0;  //Distance vue par le capteur de distance avant
+	mDistanceArriere = 0;//Distance vue par le capteur de distance arriere
+
+	mOutputPID = 0;
+
+	//Accel/Decel (0 a 5)
+	mAcceleration=0; // 0 = None et 5 = Max
+	mDecceleration=0; // 0 = Max et 5 = None
+	arretUrgence = true;
 }
 
-bool BrainControl:CalculPosition(/*State of Controler*/){
+bool BrainControl::SetArretUrgenge(){
+    arretUrgence = true;
+    return arretUrgence;
+}
+
+void BrainControl::Update(){
+    Encodeur.Update();
+    mVitesseActuel = Encodeur.GetVitesse();
+    mPositionActuel = Encodeur.GetPositionCm();
+    SetVitesseVoulu(mVitesseVoulu);
+
+
+    //100 = Vitesse Max avant
+    //0  = Vitesse Null
+    //-100   = Vitesse Max arriere
+    mOutputPID = PID.UpdatePid(mVitesseVoulu - mVitesseActuel,mVitesseActuel);
+    if(mOutputPID>100)mOutputPID =100;
+    if(mOutputPID<-100)mOutputPID =-100;
+}
+
+//_vitessevoulu en cm/sec
+void BrainControl::SetVitesseVoulu(int _vitesseVoulu)
+{
+    if(!arretUrgence)
+    {
+        if(mVitesseActuel > 0)
+        {
+            if((mPositionActuel < (mPositionMax - MAX_POSITION_LV_4))|| (mDistanceAvant > 4 && mDistanceAvant < 5) ) //Si le bolide arrive a 4m de la fin OU un objet
+            {
+                if(_vitesse > 5) _vitesse =0 TOP4;/*Set Vitesse top*/
+                else mVitesseVoulu = _vitesseVoulu; /*Set vitesse _vitesse */
+            }
+        }
+        //Repeat for mVitesseActuel < 0
+
+    }
+    else _vitesse = 0;
+}
+void BrainControl::SetPositionMax();
+
+//Set le gain P du systeme et l'output max
+void BrainControl::SetAcceleration();
+void BrainControl::SetDeceleration();
+
+bool BrainControl::CalculPosition(/*State of Controler*/){
 	return 0;
 }
--- Control/SR_Controller/BrainControl.h
+++ Control/SR_Controller/BrainControl.h
@@ -4,21 +4,30 @@
     @brief Gère le contrôle du moteur selon les interfaces externes
 
     @author Captnlink
-	
+
     @version 1.0
     @date March 20, 2016
 
 	Gère le moteur avec un PID et les informations receuillit par les capteurs (autres classes)
-	
+
 */
 
 #ifndef BRAINCONTROL_H_
 #define BRAINCONTROL_H_
 
+#include "SrEncodeur.h"
+#include "SrThermistance.h"
+#include "PidController.h"
+
+#define MAX_POSITION_LV_4 400 //En cm
+#define MAX_POSITION_LV_3 300 //En cm
+#define MAX_POSITION_LV_2 200 //En cm
+#define MAX_POSITION_LV_1 100 //En cm
+
 
 /**
     @class BrainControl BrainControl.h
-    @brief gère l'état de la LED d'information
+    @brief Gere la securite automatique et le controle de la drive
 
     @author Captnlink
     @version 1.1
@@ -26,33 +35,64 @@
 
     Gère le PID du controle de vitesse. Process les informations des capteurs et de l'interfaces
 	afin d'envoyer à la drive une consigne en tension.
-	
+
 */
 class BrainControl
 {
 private:
     //Mettre dans le .h des default config, un enum pour chaque possibilité d'erreur
-	
-	int directionChariotActuel; 
-	int directionChariotVoulu; 
-	double distance; //Distance du chariot sur le cable
-	double distanceMax; //Distance maximum du point de départ (Longueur de cable)
-	double vitesseActuel;
-	double vitesseVoulu;
-	
-	//Accel/Decel (0 a 5) 
-	int acceleration; // 0 = None et 5 = Max
-	int decceleration; // 0 = Max et 5 = None
-	
+
+	int mPositionChariotActuel;
+	int mPositionChariotVoulu;
+	int mPositionActuel; //Position du chariot sur le cable (cm)
+	int mPositionMax; //Position maximum du point de départ (Longueur de cable) (cm)
+	int mVitesseActuel; // en cm/sec
+	int mVitesseVoulu;// en cm/sec
+    int mDistanceAvant;  //Distance vue par le capteur de distance avant
+	int mDistanceArriere;//Distance vue par le capteur de distance arriere
+
+
+    //100 = Vitesse Max avant
+    //50  = Vitesse Null
+    //0   = Vitesse Max arriere
+	double mOutputPID;
+
+	//Accel/Decel (0 a 5)
+	int mAcceleration; // 0 = None et 5 = Max
+	int mDecceleration; // 0 = Max et 5 = Min
+
+	bool arretUrgence;
+
+	PidController PID;
+	SrEncodeur Encodeur;
+    Thermistance TempBatterie;
+
 public:
     BrainControl();
 
-	
+    void Update();
+
+    bool SetArretUrgenge();
+    bool IsArretUrgence(return arretUrgence);
+
 	//Setters
 	void SetVitesseVoulu();
-	void SetDistanceMax
+	void SetPositionMax();
+
+	void SetAcceleration();
+	void SetDeceleration();
 
 	//Getters
+	int GetVitesseVoulu(return mVitesseVoulu);
+	int GetVitesseActuel(return mVitesseActuel);
+
+	int GetPositionMax(return mPositionMax);
+	int GetPositionActuel(return mPositionActuel);
+
+	int GetAcceleration(return mAcceleration);
+	int GetDeceleration(return mDecceleration);
+
+	double GetTemperatureBaterie(return TempBatterie.GetTempratureCelsius());
 
 private:
     /**
@@ -63,7 +103,8 @@ private:
 
     */
 	double CalculPosition(/*directionChariotActuel + directionChariotVoulu*/);
-	
+
+
 }
 
 #endif
